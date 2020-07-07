47d0a0fca6f45438e09d7a6bf9b75a87b3fa5b28
Steve <goldenskgaming@gmail.com>
2020-07-06 17:07:16 -0700

    Initial Commit

---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Tianjiao Qiu on 7/6/20.
+//  Copyright © 2020 Tianjiao Qiu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
