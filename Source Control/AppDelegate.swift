542cc915eaf86df62deb8b80bf1cd9a0c47b7678
Steve <goldenskgaming@gmail.com>
2020-07-06 17:19:39 -0700

    Added addNumbers method.

--- Source Control/AppDelegate.swift
+++ Source Control/AppDelegate.swift
@@ -11,7 +11,16 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    /**
+     Adds two numbers together and returns the result.
+     - parameter num1: The first number.
+     - parameter num2: the second number.
+     - returns: The sum of num1 and num2
+     */
+    
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2;
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
